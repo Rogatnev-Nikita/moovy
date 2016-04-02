3c6c31a0871b0ae5f60502b0091b30c250e96dbe
Rogatnev Nikita <mail@rogatnev.ru>
2016-04-02 17:06:30 +0300

    Main menu

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -5,28 +5,72 @@ Version: 1.0.0
 Tags: 
 */
 
-
-/* ------------------------------------------ */
-/*             SCRIPT
-/* ------------------------------------------ */
+$(document).ready(function() {
 
 
-  autoPlayYouTubeModal();
+    /* ------------------------------------------ */
+    /*  MENU OVERLAY & HAMBURGER ICON ACTIVE
+    /* ------------------------------------------ */
+    $(".main-header__hamburger").click(function() {
+        if ($("body").hasClass("modal-open")) {
+            $(".modal-backdrop").fadeOut("normal", function() {
+                $(this).remove();
+            });
+            $("body").removeClass('modal-open');
+            $(".main-header__hamburger").removeClass('main-header__hamburger--active');
+        }
+        else {
+            $('<div class="modal-backdrop"></div>').fadeIn().appendTo(document.body);
+            $("body").addClass('modal-open');
+            $(".main-header__hamburger").addClass('main-header__hamburger--active');
+        }
+    });
 
-  //FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
-  function autoPlayYouTubeModal() {
-      var trigger = $("body").find('[data-toggle="modal"]');
-      trigger.click(function () {
-          var theModal = $(this).data("target"),
-              videoSRC = $(this).attr("data-theVideo"),
-              videoSRCauto = videoSRC + "?autoplay=1";
-          $(theModal + ' iframe').attr('src', videoSRCauto);
-          $(theModal + ' button.close').click(function () {
-              $(theModal + ' iframe').attr('src', videoSRC);
-          });
-          $('.modal').click(function () {
-              $(theModal + ' iframe').attr('src', videoSRC);
-          });
-      });
-  }
-  
+
+    /* ------------------------------------------ */
+    /*  LEGAL COLLAPSE - CHANGE DISPLAY OF THE BUTTON
+    /* ------------------------------------------ */
+    $(".main-footer__legal-toggle[data-toggle='collapse']").click(function() {
+        if ($(this).html() == '<i class="fa fa-chevron-up"></i> HIDE LEGAL') {
+            $(this).html('<i class="fa fa-chevron-down"></i> LEGAL');
+            $(".main-footer").css("background", "rgba(0, 0, 0, 0)");
+        }
+        else {
+            $(this).html('<i class="fa fa-chevron-up"></i> HIDE LEGAL');
+            $(".main-footer").css("background", "rgba(0, 0, 0, 1)");
+        }
+    });
+
+
+    /* ------------------------------------------ */
+    /*  MUTE BUTTON
+    /* ------------------------------------------ */
+    $("video").prop('muted', false);
+    $(".main-footer__icons-volume").click( function (){
+        if( $("video").prop('muted') ) {
+            $("video").prop('muted', false);
+            
+            $(".main-footer__icons-volume").addClass('fa-volume-up');
+            $(".main-footer__icons-volume").removeClass('fa-volume-off');
+        }
+        else {
+            $("video").prop('muted', true);
+            
+            $(".main-footer__icons-volume").addClass('fa-volume-off');
+            $(".main-footer__icons-volume").removeClass('fa-volume-up');
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

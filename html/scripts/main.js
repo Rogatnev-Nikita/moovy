0aba7128fa8d36c1103b96f79fdfb83103c01f1b
Rogatnev Nikita <mail@rogatnev.ru>
2016-04-07 21:13:54 +0300

    Video navigation

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -60,78 +60,74 @@ $(document).ready(function() {
     /*  HOME SLIDER
     /* ------------------------------------------ */
     if ($('body').hasClass('home-page')) {
-    var backgrounds = [
-        'url(./images/home/home__bg_1.jpg)',
-        'url(./images/home/home__bg_2.jpg)',
-        'url(./images/home/home__bg_3.jpg)',
-        'url(./images/home/home__bg_4.jpg)',
-        'url(./images/home/home__bg_5.jpg)',
-        'url(./images/home/home__bg_6.jpg)',
-        'url(./images/home/home__bg_7.jpg)'
-    ];
-
-    $('body').css('background-image', backgrounds[0]);
-
-    $(".main-content__slider-navigation-line:nth-child(1)").click(function() {
+        var backgrounds = [
+            'url(./images/home/home__bg_1.jpg)',
+            'url(./images/home/home__bg_2.jpg)',
+            'url(./images/home/home__bg_3.jpg)',
+            'url(./images/home/home__bg_4.jpg)',
+            'url(./images/home/home__bg_5.jpg)',
+            'url(./images/home/home__bg_6.jpg)',
+            'url(./images/home/home__bg_7.jpg)'
+        ];
         $('body').css('background-image', backgrounds[0]);
-        $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
-        $(".main-content__slider-navigation-line:nth-child(1)").addClass('main-content__slider-navigation-line--active');
-    });
-    $(".main-content__slider-navigation-line:nth-child(2)").click(function() {
-        $('body').css('background-image', backgrounds[1]);
-        $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
-        $(".main-content__slider-navigation-line:nth-child(2)").addClass('main-content__slider-navigation-line--active');
-    });
-    $(".main-content__slider-navigation-line:nth-child(3)").click(function() {
-        $('body').css('background-image', backgrounds[2]);
-        $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
-        $(".main-content__slider-navigation-line:nth-child(3)").addClass('main-content__slider-navigation-line--active');
-    });
-    $(".main-content__slider-navigation-line:nth-child(4)").click(function() {
-        $('body').css('background-image', backgrounds[3]);
-        $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
-        $(".main-content__slider-navigation-line:nth-child(4)").addClass('main-content__slider-navigation-line--active');
-    });
-    $(".main-content__slider-navigation-line:nth-child(5)").click(function() {
-        $('body').css('background-image', backgrounds[4]);
-        $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
-        $(".main-content__slider-navigation-line:nth-child(5)").addClass('main-content__slider-navigation-line--active');
-    });
-    $(".main-content__slider-navigation-line:nth-child(6)").click(function() {
-        $('body').css('background-image', backgrounds[5]);
-        $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
-        $(".main-content__slider-navigation-line:nth-child(6)").addClass('main-content__slider-navigation-line--active');
-    });
-    $(".main-content__slider-navigation-line:nth-child(7)").click(function() {
-        $('body').css('background-image', backgrounds[6]);
-        $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
-        $(".main-content__slider-navigation-line:nth-child(7)").addClass('main-content__slider-navigation-line--active');
-    });
-}
+
+        $(".main-content__slider-navigation-line:nth-child(1)").click(function() {
+            $('body').css('background-image', backgrounds[0]);
+            $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
+            $(".main-content__slider-navigation-line:nth-child(1)").addClass('main-content__slider-navigation-line--active');
+        });
+        $(".main-content__slider-navigation-line:nth-child(2)").click(function() {
+            $('body').css('background-image', backgrounds[1]);
+            $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
+            $(".main-content__slider-navigation-line:nth-child(2)").addClass('main-content__slider-navigation-line--active');
+        });
+        $(".main-content__slider-navigation-line:nth-child(3)").click(function() {
+            $('body').css('background-image', backgrounds[2]);
+            $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
+            $(".main-content__slider-navigation-line:nth-child(3)").addClass('main-content__slider-navigation-line--active');
+        });
+        $(".main-content__slider-navigation-line:nth-child(4)").click(function() {
+            $('body').css('background-image', backgrounds[3]);
+            $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
+            $(".main-content__slider-navigation-line:nth-child(4)").addClass('main-content__slider-navigation-line--active');
+        });
+        $(".main-content__slider-navigation-line:nth-child(5)").click(function() {
+            $('body').css('background-image', backgrounds[4]);
+            $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
+            $(".main-content__slider-navigation-line:nth-child(5)").addClass('main-content__slider-navigation-line--active');
+        });
+        $(".main-content__slider-navigation-line:nth-child(6)").click(function() {
+            $('body').css('background-image', backgrounds[5]);
+            $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
+            $(".main-content__slider-navigation-line:nth-child(6)").addClass('main-content__slider-navigation-line--active');
+        });
+        $(".main-content__slider-navigation-line:nth-child(7)").click(function() {
+            $('body').css('background-image', backgrounds[6]);
+            $(".main-content__slider-navigation-line").removeClass('main-content__slider-navigation-line--active');
+            $(".main-content__slider-navigation-line:nth-child(7)").addClass('main-content__slider-navigation-line--active');
+        });
+    }
 
 
 
     /* ------------------------------------------ */
     /*  VIDEO ONEPAGE SCROLL
     /* ------------------------------------------ */
-$(".main").onepage_scroll({
-   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-   easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-   animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
-   pagination: false,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-   afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-   keyboard: true,                  // You can activate the keyboard controls
-   responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
-                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-                                    // the browser's width is less than 600, the fallback will kick in.
-   direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
-});
+    if ($('body').hasClass('video-page')) {
+        $(".main").onepage_scroll({
+            sectionContainer: "section",
+            easing: "ease",
+            animationTime: 1000,
+            pagination: false,
+            updateURL: false,
+            beforeMove: function(index) {},
+            afterMove: function(index) {},
+            loop: false,
+            keyboard: true,
+            responsiveFallback: false
+        });
 
-
+    }
 
 
 

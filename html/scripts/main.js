e22d512ac36f391c306b1431f24b8f24520e40f0
Rogatnev Nikita <mail@rogatnev.ru>
2016-04-26 22:05:27 +0300

    Slider animation

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -150,7 +150,7 @@ $(document).ready(function() {
     if ($('body').hasClass('video-page') || $('body').hasClass('gallery-page') || $('body').hasClass('crew-page') || $('body').hasClass('news-page')){
         $(".main").onepage_scroll({
            sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-           easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+           easing: "cubic-bezier(0.175, 0.885, 0.420, 1.310)",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
                                             // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
            animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
            pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
